916dfc1208805ea07b2533932764d8c57f36a747
whoamins <40am1ns@protonmail.com>
2021-08-21 20:48:36 +0300

    Rename endpoints

--- main.py
+++ main.py
@@ -1,13 +1,12 @@
 from typing import List
 from fastapi import FastAPI, Path
+from starlette.responses import RedirectResponse
+
 from db.db import database, students
 from models.student import Student
 
 
 app = FastAPI()
-
-
-# TODO: Add more endpoints
 
 
 @app.on_event("startup")
@@ -20,14 +19,26 @@ async def shutdown():
     await database.disconnect()
 
 
-@app.get('/students-read/', response_model=List[Student])
+@app.get('/', include_in_schema=False)
+async def index():
+    return RedirectResponse(url='/docs')
+
+
+@app.get('/get-students/', response_model=List[Student])
 async def get_students():
     query = students.select()
-
     return await database.fetch_all(query)
 
 
-@app.post('/students-create/', response_model=Student)
+'''
+@app.get('/get-student-by-id/{student_id}', response_model=Student)
+async def get_student_by_id(student_id: int):
+    query = students.select().where(students.c.id == student_id)
+    return await database.fetch_one(query=query)
+'''
+
+
+@app.post('/create-student/', response_model=Student)
 async def create_student(student: Student):
     query = students.insert().values(name=student.name, age=student.age, group=student.group)
     last_record = await database.execute(query)
@@ -35,10 +46,9 @@ async def create_student(student: Student):
     return {**student.dict(), "id": last_record}
 
 
-@app.delete("/students/{student_id}")
+@app.delete("/delete-student/{student_id}")
 async def delete_student(student_id: int):
     query = students.delete().where(id == student_id)
     await database.execute(query)
 
-    return {"info": "Post deleted", "status_code": 204}
-
+    return {"info": "Student deleted", "status_code": 204}

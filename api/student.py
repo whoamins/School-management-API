8f0ae57890eaf16e71a8b0438b4ece6c6571063a
whoamins <40am1ns@protonmail.com>
2021-10-24 21:43:38 +0300

    Separated the main file and endpoints

---
+++ api/student.py
@@ -0,0 +1,46 @@
+from typing import List
+from fastapi import APIRouter
+from starlette.responses import RedirectResponse
+from db.db import students, database
+from models.student import Student
+
+router = APIRouter()
+
+
+@router.get('/', include_in_schema=False)
+async def index():
+    return RedirectResponse(url='/docs')
+
+
+@router.get('/get-students/', response_model=List[Student])
+async def get_students():
+    query = students.select()
+    return await database.fetch_all(query)
+
+
+@router.get('/get-students-by-group/{group}', response_model=List[Student])
+async def get_students_by_group(group: str):
+    query = students.select().where(students.c.group == group)
+    return await database.fetch_all(query)
+
+
+@router.get('/get-students-by-age/{age}', response_model=List[Student])
+async def get_students_by_age(age: int):
+    query = students.select().where(students.c.age == age)
+    return await database.fetch_all(query)
+
+
+@router.post('/create-student/', response_model=Student)
+async def create_student(student: Student):
+    query = students.insert().values(name=student.name, age=student.age, group=student.group)
+    last_record = await database.execute(query)
+
+    return {**student.dict(), "id": last_record}
+
+
+@router.delete("/delete-student/{student_id}")
+async def delete_student(student_id: int):
+    query = students.delete().where(id == student_id)
+    await database.execute(query)
+
+    return {"info": "Student deleted", "status_code": 204}
--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 from typing import List
-from fastapi import FastAPI, Path
+from fastapi import FastAPI
 from starlette.responses import RedirectResponse
 
+import api.student
 from db.db import database, students
 from models.student import Student
 
@@ -19,36 +20,4 @@ async def shutdown():
     await database.disconnect()
 
 
-@app.get('/', include_in_schema=False)
-async def index():
-    return RedirectResponse(url='/docs')
-
-
-@app.get('/get-students/', response_model=List[Student])
-async def get_students():
-    query = students.select()
-    return await database.fetch_all(query)
-
-
-'''
-@app.get('/get-student-by-id/{student_id}', response_model=Student)
-async def get_student_by_id(student_id: int):
-    query = students.select().where(students.c.id == student_id)
-    return await database.fetch_one(query=query)
-'''
-
-
-@app.post('/create-student/', response_model=Student)
-async def create_student(student: Student):
-    query = students.insert().values(name=student.name, age=student.age, group=student.group)
-    last_record = await database.execute(query)
-
-    return {**student.dict(), "id": last_record}
-
-
-@app.delete("/delete-student/{student_id}")
-async def delete_student(student_id: int):
-    query = students.delete().where(id == student_id)
-    await database.execute(query)
-
-    return {"info": "Student deleted", "status_code": 204}
+app.include_router(api.student.router, prefix='/students', tags=['Students'])
